0ae50b32e321624313d11a584b6caf042e57fb67
Mirko Akov <mupkoo@gmail.com>
2014-08-28 10:57:08 +0300

    Create the todos controller and views

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,44 @@
+class TodosController < ApplicationController
+
+    helper_method :todos
+
+    def index
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.user = current_user
+
+        if @todo.save
+            redirect_to todos_url, notice: 'New todo added.'
+        else
+            render :index
+        end
+    end
+
+    def update
+        todo.update_attributes(todo_params)
+        redirect_to todos_url, notice: 'You have updated the todo.'
+    end
+
+    def destroy
+        todo.destroy
+        redirect_to todos_url, notice: 'Todo was removed from the list'
+    end
+
+    protected
+
+    def todo_params
+        params.require(:todo).permit(:title, :is_completed)
+    end
+
+    def todo
+        @todo ||= Todo.find(params[:id])
+    end
+
+    def todos
+        @todos ||= Todo.all
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
     resources :sessions, only: [ :new, :create, :destroy ]
     resources :registrations, only: [ :new, :create ]
+    resources :todos, except: [ :show, :new, :edit ]
 
     root to: 'home#index'
 
