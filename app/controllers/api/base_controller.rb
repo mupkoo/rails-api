dc05efbc746b2233da5fd0b038c7985bc2fc5ab1
Mirko Akov <mupkoo@gmail.com>
2014-08-29 14:48:28 +0300

    Add CORS checks to the api's base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,15 +1,28 @@
 module Api
     class BaseController < ActionController::Base
 
-        after_action :set_headers
+        before_action :cors_preflight_check
+        after_action :cors_set_access_control_headers
 
         protected
 
-        def set_headers
+        def cors_preflight_check
+            if request.method == :options
+                headers['Access-Control-Allow-Origin'] = '*'
+                headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+                headers['Access-Control-Request-Method'] = '*'
+                headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token'
+                headers['Access-Control-Max-Age'] = '1728000'
+
+                render json: { success: true }
+            end
+        end
+
+        def cors_set_access_control_headers
             headers['Access-Control-Allow-Origin'] = '*'
             headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
             headers['Access-Control-Request-Method'] = '*'
-            headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+            headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token'
         end
 
     end
